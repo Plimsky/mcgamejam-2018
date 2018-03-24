c700c87553fd06b6bf39a2a6073fb785dacf1cc8
Shalin Lathigra <shalinlathigra@gmail.com>
2018-03-24 07:34:20 -0400

    Added Slight drift, causes Enemy to approach Player

--- Assets/Scripts/Environment/FollowerDeadZone.cs
+++ Assets/Scripts/Environment/FollowerDeadZone.cs
@@ -9,14 +9,32 @@ public class FollowerDeadZone : MonoBehaviour
     public float DecelerationValue = 1f;
     private float _speed;
 
+	//For Following Player
+	public float FollowThreshold;
+	public float FollowSpeed;
+	public Transform Player;
+	private float _ySpeed;
+
+	//Make the big mass follow the player.
+	//find difference of y values, have the mass slowly approach by Math.Min of deltaY, 1
+
     private void Awake()
     {
+		//For Following Player
+		Player = GameObject.FindGameObjectWithTag ("Player").transform;
         _speed = Speed;
     }
 
     private void Update()
-    {
-        transform.Translate(Vector3.right * Time.deltaTime * Speed);
+	{
+		//For Following Player
+		_ySpeed = 0.0f;
+		if (Mathf.Abs (Player.position.y - transform.position.y) > FollowThreshold)
+			_ySpeed = Mathf.Min (Player.position.y - transform.position.y, 1.0f);
+		Vector3 movementVector = Vector3.right + new Vector3 (0.0f, _ySpeed, 0.0f) * FollowSpeed / Speed;
+		transform.Translate(movementVector * Time.deltaTime * Speed);
+
+		//transform.Translate(Vector3.right * Time.deltaTime * Speed);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
