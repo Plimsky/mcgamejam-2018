fa8d50d799172fc966a5ddfb3177ce4c2e49a459
Shalin Lathigra <shalinlathigra@gmail.com>
2018-03-23 21:53:48 -0400

    Auto stash before merge of "master" and "origin/master"
    Buttons In, Working

---
+++ Assets/Scripts/MenuScripts/Quit_onClick.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Quit_onClick : MonoBehaviour {
+
+	void Start(){
+		Button b = transform.GetComponent<Button> ();
+		b.onClick.AddListener (Quit);
+	}
+
+	void Quit(){
+		Debug.Log ("A");
+		Application.Quit ();
+		Debug.Log ("B");
+	}
+}
