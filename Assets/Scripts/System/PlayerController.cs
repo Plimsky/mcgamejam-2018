cd7ca85721f97b341d5d836383ee5bb3fc677123
mngola <mikambingongola@yahoo.com>
2018-03-23 23:17:24 -0400

    Removed endless jumping bug

--- Assets/Scripts/System/PlayerController.cs
+++ Assets/Scripts/System/PlayerController.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [HideInInspector]
+    public bool jump = false;
 
+    public float speed = 2f;
+    public float moveForce = 365f;
     public float maxSpeed = 5f;
     public float jumpForce = 1000f;
-    public Transform groundCheck;
+    public LayerMask groundLayer;
 
     private bool grounded = false;
     private Animator anim;
@@ -20,18 +24,26 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+        grounded = (Physics2D.Raycast(transform.position, Vector2.down, 1.0f, groundLayer).collider != null) ? true : false;
         if (Input.GetButtonDown("Jump") && grounded)
-            Jump();
+            jump = true;
     }
 
     void FixedUpdate()
     {
-    }
+        anim.SetFloat("Speed", Mathf.Abs(speed));
 
-    void Jump()
-    {
-        anim.SetTrigger("Jump");
-        GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
+        if (speed * GetComponent<Rigidbody2D>().velocity.x < maxSpeed)
+            GetComponent<Rigidbody2D>().AddForce(Vector2.right * speed * moveForce);
+
+        if (Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) > maxSpeed)
+            GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Sign(GetComponent<Rigidbody2D>().velocity.x) * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+        if(jump)
+        {
+            anim.SetTrigger("Jump");
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
+            jump = false;
+        }
     }
 }
