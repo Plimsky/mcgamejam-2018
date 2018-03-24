2d082d615eec238b151b439d837f516c51abd677
Vincent Nicopolsky <vincent.nicopolsky@gmail.com>
2018-03-24 05:37:25 -0400

    Add new scripts for destroying a game object when the animation is played and for running animation with different offsets

---
+++ Assets/Scripts/Utils/RandomOffsetAnimation.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+    public class RandomOffsetAnimation : MonoBehaviour
+    {
+        void Awake()
+        {
+            Animator anim = GetComponent<Animator> ();
+            AnimatorStateInfo state = anim.GetCurrentAnimatorStateInfo (0);
+            anim.Play (state.fullPathHash, -1, Random.Range(0f,4f));
+        }
+    }
+}
