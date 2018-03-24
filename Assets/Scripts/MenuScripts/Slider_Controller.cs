c8c290f28b574608e351a0daf5e668bb9f5cc1db
Shalin Lathigra <shalinlathigra@gmail.com>
2018-03-24 04:02:37 -0400

    Additional UI Elements
    Added an animated Slider with Markers at 1/4, 1/2, 3/4 points to track position of player

--- Assets/Scripts/MenuScripts/Slider_Controller.cs
+++ Assets/Scripts/MenuScripts/Slider_Controller.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Slider_Controller : MonoBehaviour {
+	
+	private Slider slider;
+	private Vector3 startPos;
+	private Transform player;
+	private float fps = 20;
 
-	// Use this for initialization
+	public Transform endPos;
+	public Image relevant;
+	public Sprite[] frames;
+
 	void Start () {
-		
+		slider = transform.GetComponentInChildren<Slider> ();
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		startPos = player.position;
+		endPos = GameObject.FindGameObjectWithTag ("EndMarker").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		relevant.sprite = frames[(int)(Time.time * fps) % frames.Length];
+		Debug.Log (startPos.x + " " + player.position.x + " " + endPos.position.x);
+		slider.value = (player.position.x - startPos.x) / (endPos.position.x - startPos.x);
 	}
 }
