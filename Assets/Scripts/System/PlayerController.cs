f07bde37f74841cf2a33e9293c28f26098ca2c12
Vincent Nicopolsky <vincent.nicopolsky@gmail.com>
2018-03-24 02:18:51 -0400

    Update PlayerController for animation part, add time waiting between jumps

--- Assets/Scripts/System/PlayerController.cs
+++ Assets/Scripts/System/PlayerController.cs
@@ -12,29 +12,51 @@ public class PlayerController : MonoBehaviour
     public float maxSpeed = 5f;
     public float jumpForce = 1000f;
     public LayerMask groundLayer;
+    public float WaitTimeToRejump = 0.3f;
 
     private bool grounded = false;
+    private bool jumpable;
     private Animator anim;
 
     void Awake()
     {
         anim = GetComponent<Animator>();
+        jumpable = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        bool groundedLastFrame = grounded;
         grounded = (Physics2D.Raycast(transform.position, Vector2.down, 1.0f, groundLayer).collider != null) ? true : false;
-        if (Input.GetButtonDown("Jump") && grounded)
+
+        if (groundedLastFrame != grounded && grounded == true)
+            StartCoroutine(WaitBeforeJumpable());
+
+        if (grounded && anim.GetCurrentAnimatorStateInfo(0).IsName("PlayerFalling"))
+            anim.SetTrigger("TouchGround");
+        else
+            anim.ResetTrigger("TouchGround");
+
+        if (Input.GetButtonDown("Jump") && grounded && jumpable)
             jump = true;
+    }
+
+    private IEnumerator WaitBeforeJumpable()
+    {
+        yield return new WaitForSeconds(WaitTimeToRejump);
+        jumpable = true;
     }
 
     void FixedUpdate()
     {
         anim.SetFloat("Speed", Mathf.Abs(speed));
+        anim.SetFloat("VelocityFalling", GetComponent<Rigidbody2D>().velocity.y);
+
+        if (GetComponent<Rigidbody2D>().velocity.y < 0)
+            jumpable = false;
 
         if (speed * GetComponent<Rigidbody2D>().velocity.x < maxSpeed)
-            GetComponent<Rigidbody2D>().AddForce(Vector2.right * speed * moveForce);
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
 
         if (Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) > maxSpeed)
             GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Sign(GetComponent<Rigidbody2D>().velocity.x) * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
@@ -44,6 +66,7 @@ public class PlayerController : MonoBehaviour
             anim.SetTrigger("Jump");
             GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
             jump = false;
+            jumpable = false;
         }
     }
 }
