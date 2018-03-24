5044740e68038c9dd80d6fa57da8341cbc5b337f
Vincent Nicopolsky <vincent.nicopolsky@gmail.com>
2018-03-24 00:23:15 -0400

    Add new script that follow the player and its speed can be modified by trigger zones (through tags)

---
+++ Assets/Scripts/Environment/FollowerDeadZone.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowerDeadZone : MonoBehaviour
+{
+    public float Speed = 3f;
+    public float AccelerationValue = 2f;
+    public float DecelerationValue = 1f;
+    private float _speed;
+
+    private void Awake()
+    {
+        _speed = Speed;
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * Speed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("TriggerZoneAccelerate"))
+        {
+            Speed += AccelerationValue;
+        }
+        else if (other.gameObject.CompareTag("TriggerZoneStable"))
+        {
+            Speed = _speed;
+        }
+        else if (other.gameObject.CompareTag("TriggerZoneDecelerate"))
+        {
+            Speed -= DecelerationValue;
+        }
+    }
+}
