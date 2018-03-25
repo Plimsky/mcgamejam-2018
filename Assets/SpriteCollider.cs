685fee6166d92121a3121f9eab67daa5baa1eb9f
Unknown <monkeymanp8@gmail.com>
2018-03-25 05:11:17 -0400

    Spikes!

---
+++ Assets/SpriteCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		gameObject.AddComponent<PolygonCollider2D>(); //collider for itself
+		obj2.AddComponent<PolygonCollider2D>(); //collider for other object
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
