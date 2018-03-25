77d85954aa27e6e12fd78c51b4356a04a1c24396
mngola <mikambingongola@yahoo.com>
2018-03-24 22:35:30 -0400

    Move with player script

---
+++ Assets/Scripts/Utils/MoveWithPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWithPlayer : MonoBehaviour {
+    Vector3 lastPos;
+    public Transform player;
+
+    void Start()
+    {
+        lastPos = player.position;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        Vector3 offset = player.position - lastPos;
+        if(Mathf.Abs(offset.y) > 0.0f)
+        {
+            lastPos = player.position;
+            transform.Translate(0, offset.y, 0);
+        }
+	}
+}
